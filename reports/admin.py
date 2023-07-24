21275de57fcc3c60d1ed0a52a07f3e3ea3f03b0b
Rob Hall <r-narch@hotmail.com>
2023-07-24 15:42:51 +0100

    admin customisation

--- reports/admin.py
+++ reports/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Report
 
 # Register your models here.
-admin.site.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("User Info", {"fields": ["title", "first_name", "last_name"]}),
+        ("Report Details", {"fields": ["category", "details", "address"]}),
+    ]
+
+
+admin.site.register(Report, ReportAdmin)
