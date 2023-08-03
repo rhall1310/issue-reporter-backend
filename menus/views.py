dd215e2b7cbc35872cea724f42ef3483cb9b5186
Rob Hall <r-narch@hotmail.com>
2023-08-03 14:06:39 +0100

    permissions

--- menus/views.py
+++ menus/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
 from rest_framework import viewsets, generics
+from rest_framework.authentication import (
+    SessionAuthentication,
+    BasicAuthentication,
+    TokenAuthentication,
+)
 from .serializers import CategorySerializer
 from .models import Category
 from django_filters import rest_framework as filters
 
 # Create your views here.
 
+
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
-    filterset_fields = {'parent': ['exact', 'isnull']}
-    
-    
+    filterset_fields = {"parent": ["exact", "isnull"]}
+
 
 class TopCatViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.filter(parent__isnull=True)
-
-       
-
-
--- reports/views.py
+++ reports/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, authentication
 from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import (
+    SessionAuthentication,
+    BasicAuthentication,
+    TokenAuthentication,
+)
 from .serializers import ReportSerializer
 from .models import Report
 from django_filters import rest_framework as filters
@@ -9,6 +15,9 @@ from django_filters import rest_framework as filters
 
 
 class ReportViewSet(viewsets.ModelViewSet):
+
+    permission_classes = [IsAuthenticated]
+
     parser_classes = (FormParser, MultiPartParser)
     serializer_class = ReportSerializer
     queryset = Report.objects.all()
