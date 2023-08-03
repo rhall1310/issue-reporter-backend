d49dbda9993038837c9508422f7e35ca26d1b6ec
Rob Hall <r-narch@hotmail.com>
2023-08-03 14:13:19 +0100

    redundant permissions removed

--- reports/views.py
+++ reports/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-from rest_framework import viewsets, generics, authentication
+from rest_framework import viewsets, generics
 from rest_framework.parsers import FormParser, MultiPartParser
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import (
-    SessionAuthentication,
-    BasicAuthentication,
-    TokenAuthentication,
-)
+
+
 from .serializers import ReportSerializer
 from .models import Report
 from django_filters import rest_framework as filters
@@ -15,8 +11,6 @@ from django_filters import rest_framework as filters
 
 
 class ReportViewSet(viewsets.ModelViewSet):
-
-    permission_classes = [IsAuthenticated]
 
     parser_classes = (FormParser, MultiPartParser)
     serializer_class = ReportSerializer
